d3c36235ee9b142b556445258fcefc1a66e723a4
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-08-12 02:38:01 +0530

    Add Appwrite SDK integration for search term tracking; update App component to call updateSearchCount on successful movie fetch.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
 import MovieCard from './components/MovieCard.jsx';
 import { useDebounce } from 'react-use';
+import { updateSearchCount } from './components/appwrite.js';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3'
 
@@ -22,12 +23,13 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState('');
   const [movieList, serMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [debouncedSearchTerm,setDebouncedSearchTerm] = useState('');
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+
 
 
   // Debounce the search term to prevent making too many API requests
   // by waiting for the user to stop typing for 500ms
-  useDebounce(()=> setDebouncedSearchTerm(searchTerm),500,[searchTerm])
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
 
   const fetchMovies = async (query = '') => {
 
@@ -55,6 +57,11 @@ const App = () => {
       }
 
       serMovieList(data.results || []);
+
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
+
 
     } catch (error) {
       console.error(`error fetching movies ${error}`);
